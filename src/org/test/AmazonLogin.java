420a2c3979886f80c5b38a6449729f71a4e4e7be
Rohini1410 <rohinijayaraman1496@gmail.com>
2021-07-02 01:24:34 +0530

    Amazon login module piushed in repo in the github

---
+++ src/org/test/AmazonLogin.java
@@ -0,0 +1,20 @@
+package org.test;
+
+public class AmazonLogin {
+	
+	private void Login() {
+	
+		System.out.println("Launch url");
+		System.out.println("Enter user name and Password");
+		System.out.println("Click login");
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		AmazonLogin l = new AmazonLogin();
+		l.Login();
+		
+	}
+
+}
